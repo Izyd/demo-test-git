55ca50e7de60101bb83a1a7c932a8e1570586b01
Izyd <gysko2109@gmail.com>
2016-11-28 16:31:30 -0500

    Ajout d'un fichier php test

---
+++ test.php
@@ -0,0 +1,32 @@
+
+
+ <?php
+
+class Personnage
+
+{
+
+  private $_experience;
+
+        
+
+  public function gagnerExperience()
+
+  {
+
+    // Cette méthode doit ajouter 1 à l'expérience du personnage.
+
+  }
+
+}
+
+
+$perso = new Personnage;
+
+$perso->gagnerExperience();
+}
+
+
+$perso = new Personnage;
+
+$perso->gagnerExperience();
